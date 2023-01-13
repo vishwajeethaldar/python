a93edfb5745e78f86530fa5cf9ca2396b81c478c
vishwajeethaldar <onlyjeet3@gmail.com>
2023-01-13 17:15:48 +0530

    dd

--- Lecture_3_DJango/airline/flights/views.py
+++ Lecture_3_DJango/airline/flights/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Flights
 # Create your views here.
 def index(request):
     return render(request, "flights/index.html", {
-        "flights":Flights.objects.all()
+        "flightlist":Flights.objects.all()
     })
+
+def flight(request, flight_id) :
+    flightInfo  =  Flights.objects.get(id=flight_id)
+    # or  flightInfo  =  Flights.objects.get(pk=flight_id)
+   
+    return render(request, "flights/flight.html", {
+        "flight":flightInfo
+    })
+    # return HttpResponse(flightInfo.origin)
